6191d37282be3c7327adb82444536ce95f60502d
DalioSY <dalioyamada@gmail.com>
2024-06-26 15:34:34 -0300

    teste

--- cards.js
+++ cards.js
@@ -2,7 +2,7 @@ const cards = document.querySelector(".cards");
 let myArticle = "";
 
 arrayCards.forEach((card) => {
-  myArticle += `
+	myArticle += `
             <div class="card">
                 <img src=${card.src} alt="Foto projeto" width="200px">
                 <h3 class="title-card">${card.title}</h3>
